e9cdc7d5571df57766ef530baf2c7cfc99d4fed9
Khanhke2210 <khanh.le.set18@eiu.edu.vn>
2023-03-15 10:20:58 +0700

    GUI Completion

---
+++ XTCommunity20/app/src/main/java/com/example/xtcommunity20/SinglePostActivity.java
@@ -0,0 +1,14 @@
+package com.example.xtcommunity20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinglePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_post);
+    }
+}
